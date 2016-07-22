2ca2129fd173b3af4081ba75ba98f9051d1ca37f
Elessog <elouan.autret@ensta-bretagne.org>
2016-07-22 09:19:39 +0300

    add stop script for proper stopping of simulation

--- simu_run.sh
+++ simu_run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+
+function clean_up {
+
+	# Perform program exit housekeeping
+
+  ps -ef | grep "/sr ./sailing_simulator/ simu_asr.db" | grep -v grep | awk '{print $2}' | xargs kill -9 --verbose
+
+  sleep 3
+  ps -ef | grep gpsd  | grep -v grep | awk '{print $2}' | xargs kill -9 --verbose
+  ps -ef | grep socket_to_sr  | grep -v grep | awk '{print $2}' | xargs kill -s SIGINT --verbose
+	exit
+}
+
+
+
 SIMU_PATH=./PROGSIDE
 
 sh -c "$SIMU_PATH/socket_to_sr $SIMU_PATH/port_path.sh" &
@@ -19,4 +34,7 @@ sqlite3 simu_asr.db "UPDATE windsensor_config SET port='$CV7_PORT_SIMU';"
 
 sleep 1
 
-cd ../ && LD_PRELOAD=sailing_simulator/PROGSIDE/libwiringPiH.so ./sr ./sailing_simulator/ simu_asr.db
+cd ../ && LD_PRELOAD=sailing_simulator/PROGSIDE/libwiringPiH.so ./sr ./sailing_simulator/ simu_asr.db &
+
+trap clean_up SIGINT SIGTERM SIGHUP
+wait %1
