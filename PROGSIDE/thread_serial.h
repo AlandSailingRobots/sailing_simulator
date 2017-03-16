ffa79ec348bdc8adc560e5d8d8acf169099ee35e
Jordan Less'ard-Springet <jordan80100@gmail.com>
2017-03-16 09:40:53 +0200

    Reorganised and removed old scripts

--- PROGSIDE/thread_serial.h
+++
@@ -1,9 +0,0 @@
-#ifndef THREAD_SERIAL
-#define THREAD_SERIAL
-
-void *gps_thread(void *thread_handler);
-void *cv7_thread(void *thread_handler);
-void *maestro_thread(void *thread_handler);
-
-
-#endif
